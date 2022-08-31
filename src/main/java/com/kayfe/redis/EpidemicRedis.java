70a6c82da02b1f0a1f85439d456ee4987ff41f26
linkaifei <1186099110@qq.com>
2022-08-31 14:47:48 +0800

    update redis

--- src/main/java/com/kayfe/redis/EpidemicRedis.java
+++ src/main/java/com/kayfe/redis/EpidemicRedis.java
@@ -5,8 +5,7 @@ import redis.clients.jedis.JedisPool;
 
 public class EpidemicRedis {
 
-    private static JedisPool pool = new JedisPool("127.0.0.1",5345);
-//    private static JedisPool pool = new JedisPool("175.178.183.118",5345);
+    private static JedisPool pool = new JedisPool("127.0.0.1",6379);
     public static Jedis getJedis(){
         return pool.getResource();
     }
